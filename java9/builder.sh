c11b05506498ba6d853446f66bd9ba40a83b009d
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-07-19 23:35:28 +0200

    updating builder.sh script

--- java9/builder.sh
+++ java9/builder.sh
@@ -5,38 +5,39 @@ export BUILDER_OUT_DIR="out/production/java9/"
 export BUILDER_SRC_FILE="src/com/github/pwittchen/Main.java"
 export BUILDER_MAIN_CLASS="com.github.pwittchen.Main"
 
-function showHelp() {
+function help() {
+  echo ":help"
   echo "
-        builder.sh is a simple shell script responsible
-        for cleaning, building and running this Java 9 application
+     builder.sh is a simple shell script responsible
+     for cleaning, building and running this Java 9 application
 
-        Usage:
-          -c    cleans the project (removes 'out/' directory)
-          -b    builds (compiles) the project
-          -r    runs the compiled project
-          -h    shows help
-        "
+     Usage:
+       -c    cleans the project (removes 'out/' directory)
+       -b    builds (compiles) the project
+       -r    runs the compiled project
+       -h    shows help
+  "
 }
 
 function clean() {
+  echo ":clean"
   rm -rf $BUILDER_OUT_DIR
-  echo "project was cleaned"
 }
 
 function build() {
+  echo ":build"
   $BUILDER_JVM_PATH/bin/javac -d $BUILDER_OUT_DIR $BUILDER_SRC_FILE
-  echo "project was built"
 }
 
 function run() {
-  echo "running the project:"
+  echo ":run"
   $BUILDER_JVM_PATH/bin/java -Dfile.encoding=UTF-8 -classpath $BUILDER_OUT_DIR $BUILDER_MAIN_CLASS
 }
 
 while getopts "hcbr" opt; do
     case "$opt" in
     h)
-        showHelp
+        help
         exit 0
         ;;
     c)  clean
