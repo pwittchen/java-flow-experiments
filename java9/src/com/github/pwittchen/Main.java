a621f58844a89c387faa7778815bff9711626d8c
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-07-19 18:07:53 +0200

    adding exemplary Publisher and Subscriber for Java9

--- java9/src/com/github/pwittchen/Main.java
+++ java9/src/com/github/pwittchen/Main.java
@@ -1,7 +1,31 @@
 package com.github.pwittchen;
 
+import java.util.concurrent.Flow;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello Java 9");
+
+    Flow.Publisher<Integer> publisher = subscriber -> {
+      subscriber.onNext(7);
+      subscriber.onComplete();
+    };
+
+    publisher.subscribe(new Flow.Subscriber<>() {
+      @Override public void onSubscribe(Flow.Subscription subscription) {
+        System.out.println("onSubscribe");
+      }
+
+      @Override public void onNext(Integer item) {
+        System.out.println("onNext: ".concat(item.toString()));
+      }
+
+      @Override public void onError(Throwable throwable) {
+        System.out.println("onError: ".concat(throwable.getMessage()));
+      }
+
+      @Override public void onComplete() {
+        System.out.println("onComplete");
+      }
+    });
   }
 }
