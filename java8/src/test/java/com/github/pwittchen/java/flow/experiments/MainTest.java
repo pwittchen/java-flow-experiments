bf9ebec1acfcf1babd4fff813b9c315c689f021b
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-09-19 12:02:16 +0200

    adding akka stream dependency and test class

---
+++ java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.pwittchen.java.flow.experiments;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+  @Test
+  public void shouldMergeStreamsFromDifferentApis() {
+    //TODO: implement
+  }
+}
