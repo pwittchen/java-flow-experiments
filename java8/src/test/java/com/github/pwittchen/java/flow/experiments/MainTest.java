48c9774331faa923c617bacd1deb52000e4c2a39
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-09-19 12:34:44 +0200

    adding first test

--- java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
+++ java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
@@ -1,13 +1,30 @@
 package com.github.pwittchen.java.flow.experiments;
 
+import io.reactivex.Flowable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 
-import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertThat;
 
 public class MainTest {
 
   @Test
   public void shouldMergeStreamsFromDifferentApis() {
-    //TODO: implement
+    // given
+    List<Integer> expectedResult = IntStream.range(1, 9).boxed().collect(Collectors.toList());
+    List<Integer> actualResult = new ArrayList<>();
+
+    Flowable<Integer> flowable = Flowable.just(1, 2, 3, 4);
+    Flux<Integer> flux = Flux.just(5, 6, 7, 8);
+
+    // when
+    flowable.mergeWith(flux).sorted().subscribe(actualResult::add);
+
+    // then
+    assertThat(actualResult).isEqualTo(expectedResult);
   }
 }
