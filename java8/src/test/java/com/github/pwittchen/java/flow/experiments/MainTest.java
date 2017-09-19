5922601f2132534f92b48287f46edbfc35894155
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-09-19 14:53:24 +0200

    adding samples with callbacks and flatmap

--- java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
+++ java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
@@ -6,21 +6,19 @@ import io.reactivex.ObservableTransformer;
 import io.reactivex.schedulers.Schedulers;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
 import static com.google.common.truth.Truth.assertThat;
 
 public class MainTest {
-
   @Test
   public void shouldMergeStreamsFromDifferentApis() {
     // given
-    List<Integer> expectedResult = IntStream.range(1, 9).boxed().collect(Collectors.toList());
+    List<Integer> expectedResult = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
     List<Integer> actualResult = new ArrayList<>();
 
     Flowable<Integer> flowable = Flowable.just(1, 2, 3, 4);   // RxJava2
@@ -35,7 +33,69 @@ public class MainTest {
 
   @Test
   public void shouldDoTheSameJobAsImperativeCodeInReactiveWay() {
-    //TODO: implement
+    // given
+    final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+    final List<Integer> evenNumbersImperative = new ArrayList<>();
+    final List<Integer> evenNumbersReactive = new ArrayList<>();
+
+    // when
+
+    // imperative way
+    for (Integer n : list) {
+      if (n % 2 == 0) {
+        evenNumbersImperative.add(n);
+      }
+    }
+
+    // reactive way
+    Flowable.fromIterable(list)
+        .filter(n -> n % 2 == 0)
+        .subscribe(evenNumbersReactive::add);
+
+    // then
+    assertThat(evenNumbersImperative).isEqualTo(evenNumbersReactive);
+  }
+
+  @Test
+  public void shouldPerformOneOperationAfterAnother() {
+    // given
+    final List<String> operationsWithCallbacks = new ArrayList<>();
+    final List<String> operationsReactive = new ArrayList<>();
+
+    // when
+
+    // with callbacks
+
+    new Thread(() -> ((Callback) () -> {
+      operationsWithCallbacks.add("operation 1");
+      ((Callback) () -> {
+        operationsWithCallbacks.add("operation 2");
+        ((Callback) () -> {
+          operationsWithCallbacks.add("operation 3");
+        }).execute();
+      }).execute();
+    }).execute()).start();
+
+    // reactive way
+
+    final Flowable<String> first = Flowable.fromCallable(() -> "operation 1");
+    final Flowable<String> second = Flowable.fromCallable(() -> "operation 2");
+    final Flowable<String> third = Flowable.fromCallable(() -> "operation 3");
+
+    first.flatMap(name -> {
+      operationsReactive.add(name);
+      return second;
+    }).flatMap(name -> {
+      operationsReactive.add(name);
+      return third;
+    }).subscribe(operationsReactive::add);
+
+    assertThat(operationsWithCallbacks).isEqualTo(operationsReactive);
+  }
+
+  @FunctionalInterface
+  private interface Callback {
+    void execute();
   }
 
   @Test
