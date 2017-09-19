00cbd9f4f07cda082b4dae7c247abaea6debe693
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-09-19 12:50:49 +0200

    adding next test for checking execution time with schedulers

--- java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
+++ java8/src/test/java/com/github/pwittchen/java/flow/experiments/MainTest.java
@@ -1,8 +1,13 @@
 package com.github.pwittchen.java.flow.experiments;
 
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.schedulers.Schedulers;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.Test;
@@ -26,5 +31,59 @@ public class MainTest {
 
     // then
     assertThat(actualResult).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void shouldPerformCalculationWithComputationScheduler() {
+    Observable.range(1, 10)
+        .doFinally(this::sleepForAWhile)
+        .flatMap(integer -> Observable.just(integer)
+            .subscribeOn(Schedulers.computation())
+            .map(this::intenseCalculation))
+        .compose(applyBenchmarkWithAssertion(8))
+        .subscribe(
+            number -> {
+              String format =
+                  String.format("Received %d %s on thread %s", number, LocalTime.now().toString(),
+                      Thread.currentThread().getName());
+              System.out.println(format);
+            });
+  }
+
+  private <T> T intenseCalculation(T value) {
+    sleep(ThreadLocalRandom.current().nextInt(3000));
+    return value;
+  }
+
+  private <T> ObservableTransformer<T, T> applyBenchmark() {
+    return applyBenchmarkWithAssertion(null);
+  }
+
+  private <T> ObservableTransformer<T, T> applyBenchmarkWithAssertion(
+      final Integer maxExpectedComputationTime) {
+    // if we want to use local variable for transformer, it needs to be an array
+    final long[] executionTime = new long[2];
+    return upstream -> upstream
+        .doOnSubscribe(disposable -> executionTime[0] = System.currentTimeMillis())
+        .doOnComplete(() -> {
+          executionTime[1] = System.currentTimeMillis();
+          long computationTime = (executionTime[1] - executionTime[0]) / 1000;
+          System.out.println(String.format("computed in %d seconds", computationTime));
+          if (maxExpectedComputationTime != null) {
+            assertThat(computationTime).isLessThan(maxExpectedComputationTime.longValue());
+          }
+        });
+  }
+
+  private void sleepForAWhile() {
+    sleep(10000);
+  }
+
+  private void sleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
