2d1240d03156f8e0674268f43b144fd5b10bb8ac
Piotr Wittchen <piotr.wittchen@sap.com>
2017-09-26 19:47:48 +0200

    moving cast to Pipe class

--- java9/src/com/github/pwittchen/Main.java
+++ java9/src/com/github/pwittchen/Main.java
@@ -48,7 +48,7 @@ public class Main {
     new Pipe()
         .of(Stream.of(1, 2, 3, 4, 5, 6))
         .filter(o -> (Integer) o % 2 == 0)
-        .subscribe((Consumer) System.out::println);
+        .subscribe(System.out::println);
   }
 }
 
@@ -67,5 +67,9 @@ class Pipe implements Flow.Publisher {
 
   @Override public void subscribe(final Flow.Subscriber subscriber) {
     stream.forEach(subscriber::onNext);
+  }
+
+  public void subscribe(final Consumer consumer) {
+    subscribe((Flow.Subscriber) consumer);
   }
 }
